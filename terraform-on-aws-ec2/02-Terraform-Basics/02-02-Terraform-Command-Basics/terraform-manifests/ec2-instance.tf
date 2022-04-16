b68a12ed78ef7ddcf24c69be40e6a8046a770cdb
Rizky Indiarto <rizky.indiarto@brank.as>
2022-04-17 01:19:34 +0700

    initialize terraform project commit

---
+++ terraform-on-aws-ec2/02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -0,0 +1,22 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0e1d30f2c40c4c701" # user that we used
+  instance_type = "t2.micro" # what type instance that we use
+  
+}
